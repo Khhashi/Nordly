aedc7eb7ba48a8ec987f6f0316b1154a657d87ac
Khalid Hashi <139186296+Khhashi@users.noreply.github.com>
2026-09-18 11:18:46 +0200

    Support Render PostgreSQL connection URLs
    Konverterer Render postgres:// URL til Npgsql-format. Tester: 21/21 og GitHub Actions grønn.

---
+++ PG3302.Tests/DatabaseConnectionStringTests.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+using OrderManager.Web;
+
+namespace PG3302.Tests;
+
+public class DatabaseConnectionStringTests
+{
+    [Test]
+    public void Normalize_Should_Convert_Render_Postgres_Url()
+    {
+        var result = DatabaseConnectionString.Normalize(
+            "postgres://render_user:secret%40value@dpg.example.com:5432/nordly");
+
+        Assert.That(result, Does.Contain("Host=dpg.example.com"));
+        Assert.That(result, Does.Contain("Port=5432"));
+        Assert.That(result, Does.Contain("Database=nordly"));
+        Assert.That(result, Does.Contain("Username=render_user"));
+        Assert.That(result, Does.Contain("Password=secret@value"));
+        Assert.That(result, Does.Contain("SSL Mode=Require"));
+    }
+
+    [Test]
+    public void Normalize_Should_Keep_Npgsql_Connection_String()
+    {
+        const string connectionString = "Host=localhost;Port=5432;Database=nordly";
+
+        Assert.That(DatabaseConnectionString.Normalize(connectionString), Is.EqualTo(connectionString));
+    }
+}
